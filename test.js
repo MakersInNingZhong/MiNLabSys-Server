e918349228bb3dd5685d4012f3a55b4ad050a21f
LEAFERx <jerryleafer@outlook.com>
2017-05-13 10:59:03 +0800

    test

--- test.js
+++ test.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var port = 3000;
+var mysql = require('mysql');
+var dbinfo = require("./dbinfo.json")
 
-app.post("/", function(req, res){
-    res.send(200, req.body);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: dbinfo.user,
+    password: dbinfo.passwd,
+    database: dbinfo.db
+});
+
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    connection.connect();
+    connection.query('select * from member where name='+req.body.name, function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows[0]);
+    });
+    connection.end();
+    res.send("ok");
 });
 
 app.listen(port);
